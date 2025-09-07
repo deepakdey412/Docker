a238d1fd7ec9ba5cff50cecc4f6731dfa4dc1c26
Deepak Dey <deepakdey412@gmail.com>
2025-09-07 20:05:13 +0530

    Add project with docker

---
+++ 08 - Docker Best Practices With Project/Simple-Crud/src/main/java/com/Simple_Crud/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Simple_Crud.repository;
+
+import com.Simple_Crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
